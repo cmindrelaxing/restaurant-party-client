dd111deda7bfb598d2fc57b7ed77bebbf854ba69
prographicsten <prographicsten@gmail.com>
2023-11-06 12:51:37 +0600

    add a new collection pages codes 50% complete now new connect mongodb

--- src/pages/AddProducts.jsx
+++ src/pages/AddProducts.jsx
@@ -1,47 +1,24 @@
 
-// const AddProducts = () => {
-//     return (
-//         <div>
-//             Add Products
-//         </div>
-//     );
-// };
-
-// export default AddProducts;
-
-
 import '../index.css';
-import { Link } from "react-router-dom";
-
-
 
 const AddProducts = () => {
 
-    const signup = e => {
+    const addNewItem = e => {
         e.preventDefault();
 
         const form  = e.target;
         const name = form.name.value;
-        const email = form.email.value;
-        const password = form.password.value;
-        const signupInfo = {name, email, password};
-        console.log(signupInfo);
+        const price = form.price.value;
+        const image = form.image.value;
+        const category = form.category.value;
+        const description = form.description.value;
 
-        // Validate email and password
-        
+        const productInfo = {name, price, image, category, description};
+        console.log(productInfo);
     };
 
-
-    // google sign up
-    
-
-
-    // github sign up page
-    
-    
-
     return (
-        <div className=" min-h-screen bg-base-200 flex items-center justify-center py-10 ">
+        <div className=" min-h-screen bg-base-200 flex items-center justify-center py-20 ">
             <div className="hero-content w-full flex-col-reverse md:flex-row justify-center">
 
                 <div className="text-center mb-2 mt-16 md:mt-0">
@@ -50,44 +27,48 @@ const AddProducts = () => {
                 </div>
 
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                    <form onSubmit={signup} className="card-body" data-aos="flip-right">
+                    <form onSubmit={addNewItem} className="card-body" data-aos="flip-right">
+                        <h2 className="text-2xl font-semibold text-center dancing">Add a New Product</h2>
+
                         <div className="form-control">
                         <label className="label">
-                            <span className="label-text">Name</span>
+                            <span className="label-text dancing font-bold text-lg">Food Name</span>
                         </label>
-                        <input type="text" placeholder="name" name="name" className="focus:outline-none focus:border-[2px] focus:border-[#5dff33] focus:text-[#5dff33] input input-bordered" required />
+                        <input type="text" placeholder="Name" name="name" className=" text-xs focus:outline-none focus:border-[2px] focus:border-[#5dff33]  input input-bordered" required />
                         </div>
 
                         <div className="form-control">
                         <label className="label">
-                            <span className="label-text">Email</span>
+                            <span className="label-text dancing font-bold text-lg">Food Price</span>
                         </label>
-                        <input type="email" placeholder="email" name="email" className="focus:outline-none focus:border-[2px] focus:border-[#5dff33] focus:text-[#5dff33] input input-bordered" required />
+                        <input type="text" placeholder="Price" name="price" className=" text-xs focus:outline-none focus:border-[2px] focus:border-[#5dff33]  input input-bordered" required />
                         </div>
 
                         <div className="form-control">
                         <label className="label">
-                            <span className="label-text">Password</span>
+                            <span className="label-text dancing font-bold text-lg">Food Image</span>
                         </label>
-                        <input type="password" placeholder="password" name="password" className="focus:outline-none focus:border-[2px] focus:border-[#5dff33] focus:text-[#5dff33] input input-bordered" required />
+                        <input type="text" placeholder="Example: https://image.jpg" name="image" className=" text-xs focus:outline-none focus:border-[2px] focus:border-[#5dff33]  input input-bordered" required />
+                        </div>
+
+                        <div className="form-control">
                         <label className="label">
-                            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                            <span className="label-text dancing font-bold text-lg">Food Category</span>
                         </label>
+                        <input type="text" placeholder="Category..." name="category" className=" text-xs focus:outline-none focus:border-[2px] focus:border-[#5dff33]  input input-bordered" required />
+                        </div>
+
+                        <div className="form-control">
+                        <label className="label">
+                            <span className="label-text dancing font-bold text-lg">Food Description</span>
+                        </label>
+                        <textarea  className=' text-xs rounded-lg border-2 focus:outline-none p-4 focus:border-[2px] focus:border-[#5dff33]' name="description" id="" placeholder='Description...' cols="30" rows="5"></textarea>
                         </div>
 
                         <div className="form-control mt-6">
-                        <button className="btn text-white py-2 bg-[#5dff33] hover:bg-black hover:text-white rounded-full">Signup</button>
+                        <button className="btn text-white py-2 bg-[#5dff33] capitalize hover:bg-black hover:text-white rounded-full">Add new Collection</button>
                         </div>
 
-                        <p className="text-center mt-4"><small>Already Have an account <Link to="/login" className="text-blue-600 font-bold">Login</Link></small></p>
-
-                        <div>
-                            <h2 className="text-2xl font-semibold text-center dancing">Signup with</h2>
-                            <div className="flex justify-between items-center mt-5 gap-3">
-                                <button className="flex items-center font-medium bg-[#5dff33] text-white py-2 hover:bg-black hover:text-white px-8 rounded-full"><i className='mr-2 bx bxl-google text-black' ></i><span className="text-black">G</span>oogle</button>
-                                <button className="flex items-center font-medium bg-[#5dff33] text-white py-2 hover:bg-black hover:text-white px-8 rounded-full"><i className='mr-2 bx bxl-github text-black' ></i><span className="text-black">G</span>ithub</button>
-                            </div>
-                        </div>
                     </form>
                 </div>
             </div>
