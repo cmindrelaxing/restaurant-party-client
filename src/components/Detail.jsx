008c21fd045b404c14796386357059330002b4ed
prographicsten <prographicsten@gmail.com>
2023-11-07 21:22:44 +0600

    order count something complete

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -1,5 +1,6 @@
 
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 import toast from 'react-hot-toast';
 
 const Detail = ({details}) => {
@@ -7,9 +8,15 @@ const Detail = ({details}) => {
     console.log(details);
     const { name, price, image, category, description, made, country } = details || {};
 
+    const [countOrder, setCountOrder] = useState(0);
+
     const orderConfirm = () => {
+        const countInfo = countOrder + 1;
+        setCountOrder(countInfo);
         toast.success('Successfully toasted!')
     };
+
+    console.log(countOrder);
 
     return (
         <div className="py-20 space-y-5">
@@ -41,8 +48,11 @@ const Detail = ({details}) => {
                     <p className='font-semibold'>{category}</p>
                 </div>
 
-                <div>
+                <div className='flex items-center gap-10'>
                     <button onClick={orderConfirm} className="bg-[#5dff33] text-white py-2 font-semibold capitalize hover:bg-orange-400 hover:text-white px-6 md:px-8 rounded-full">Order Confirm</button>
+                    <div className=' bg-[#5dff33] w-10 h-10 flex justify-center items-center rounded-full'>
+                        <p className='capitalize font-bold dancing text-white'><small>{countOrder}</small></p>
+                    </div>
                 </div>
             </div>
         </div>
