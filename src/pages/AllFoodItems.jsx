02737f77f6b14937232a828b5537993d9c46c52a
prographicsten <prographicsten@gmail.com>
2023-11-08 10:50:30 +0600

    start pagination

--- src/pages/AllFoodItems.jsx
+++ src/pages/AllFoodItems.jsx
@@ -2,13 +2,64 @@
 import { useLoaderData } from 'react-router-dom';
 import '../index.css';
 import FoodItem from '../components/FoodItem';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const AllFoodItems = () => {
 
     const loadedFoods = useLoaderData();
     // console.log(foods);
+
     const [foods, setFoods] = useState(loadedFoods);
+    const [foodsCount, setFoodsCount] = useState();
+    const {count} = foodsCount || {};
+    console.log(count);
+
+    const itemsPerPage = 9;
+    const numberOfPages = Math.ceil(count / itemsPerPage);
+    console.log(numberOfPages);
+
+    // can i load more pages by for looping or Array
+    const pages = [];
+    for (let i = 0; i < numberOfPages; i++) {
+        pages.push(i);
+    }
+    
+    // Array of pages
+    // const pages = [...Array(numberOfPages).keys()];
+    // console.log(pages);
+
+
+    // pagination for foods count
+    useEffect(() => {
+        fetch(`http://localhost:5000/foodsCount`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            setFoodsCount(data);
+        })
+    }, []);
+    
+
+    // async function with pagination
+    // useEffect(() => {
+    //     const fetchData = async () => {
+    //         try {
+    //             const response = await fetch("http://localhost:5000/foods");
+    //             if (response.ok) {
+    //                 const data = await response.json();
+    //                 console.log(data);
+    //                 setFoodsCount(data);
+    //             } else {
+    //                 console.error("Failed to fetch data");
+    //             }
+    //         } catch (error) {
+    //             console.error("An error occurred: ", error);
+    //         }
+    //     };
+    
+    //     fetchData();
+    // }, []);
+
 
     return (
         <div className='pb-20 pt-10'>
@@ -21,6 +72,12 @@ const AllFoodItems = () => {
                 <div className=' grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 area mx-auto md:px-5 lg:px-0'>
                     {
                         foods?.map(food => <FoodItem key={food._id} food={food} foods={foods} setFoods={setFoods}></FoodItem>)
+                    }
+                </div>
+                <div className='text-center mt-10 pagination'>
+                    {
+                        // pages?.map((page, index) => <button className='btn rounded-sm ml-1 mr-1 ' key={index}>{page}</button>)
+                        pages?.map((page, index) => <button className='text-base text-white bg-[#5dff33] hover:bg-orange-500 w-10 h-10 ml-1 mr-1 rounded-sm' key={index}>{page}</button>)
                     }
                 </div>
             </div>
