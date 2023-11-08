90b6251da5c2861121bb08899fe509c981c6fff0
prographicsten <prographicsten@gmail.com>
2023-11-08 21:33:23 +0600

    now choice page add

--- src/pages/ChoiceFood.jsx
+++ src/pages/ChoiceFood.jsx
@@ -10,8 +10,15 @@ const ChoiceFood = () => {
     // console.log(foods);
 
     return (
-        <div>
-            <div className='area mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 py-20'>
+        <div className=' py-10'>
+            <div>
+                <h2 className='text-center dancing font-bold text-3xl  mb-5'>Search..</h2>
+                <div className='flex justify-center items-center w-1/2 mx-auto mb-14'>
+                    <input className=' flex-1 py-2 px-4 border-r-0 rounded-tl-full rounded-bl-full border-2 focus:outline-none focus:border-[2px] focus:border-[#5dff33] focus:text-[#5dff33]' type="search" name="" id="" />
+                    <i className=' bg-[#5dff33] text-white py-2 px-5 rounded-tr-full rounded-br-full text-lg bx bx-search-alt-2'></i>
+                </div>
+            </div>
+            <div className='area mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10'>
                 {
                     foods?.map(food => <ChoiceOne key={food._id} food={food}></ChoiceOne>)
                 }
